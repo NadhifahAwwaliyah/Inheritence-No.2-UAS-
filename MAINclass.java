878710026fea848d548577e62dc891e756b5e31d
NadhifahAwwaliyah <nadhifah.21060@mhs.unesa.ac.id>
2022-12-29 18:45:01 +0700

    first commit

---
+++ MAINclass.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class MAINclass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[])
+    {
+        engineerr E1 = new engineerr();
+        employ Employee = new employ();
+        
+        System.out.println ("Salary : " + E1.salary
+                           + "\nBenefits : " + E1.benefits);
+    }  
+}
